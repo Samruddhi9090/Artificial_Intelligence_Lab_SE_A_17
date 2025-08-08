a1cbd80bddb2e4e3fe011a7cbdcdfc889a591e09
Aartikale23 <aarti.kale24@mmit.edu.in>
2025-08-08 09:58:49 +0530

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,13 @@
+# Python program to check if a number is positive, negative, or zero
+
+# Take input from the user
+num = float(input("Enter a number: "))
+
+# Check conditions using if-elif-else
+if num > 0:
+    print("The number is positive.")
+elif num == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
+
